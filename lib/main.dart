22ee8fa2b6671fa8f1967f8f8ffa3b6bb22c934f
derco19 <harshvardhanagarwal159@gmail.com>
2020-10-15 22:55:24 +0530

    Started Making Chart

--- lib/main.dart
+++ lib/main.dart
@@ -54,18 +54,18 @@ class _MyHomePageState extends State<MyHomePage> {
   final amountController = TextEditingController();
 
   final List<Transaction> _userTransaction = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.95,
-      date: DateTime.now(),
-    ),
+//    Transaction(
+//      id: 't1',
+//      title: 'New Shoes',
+//      amount: 69.99,
+//      date: DateTime.now(),
+//    ),
+//    Transaction(
+//      id: 't2',
+//      title: 'Weekly Groceries',
+//      amount: 16.95,
+//      date: DateTime.now(),
+//    ),
   ];
 
   List<Transaction> get _recentTransaction {
